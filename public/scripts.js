edfc1aad85bd0187f515ca5a05da855d79085639
Rodrigo Emanuel <drigosantos@gmail.com>
2020-04-04 12:55:28 -0300

    Validação da entrada de dados no formulário de cadastro de novas ideias.

--- public/scripts.js
+++ public/scripts.js
@@ -6,3 +6,36 @@ function onOff(){
     document.querySelector("modal").classList.toggle("addScroll");
 }
 
+function checkFields(event) {
+    const valuesToCheck = [
+        "title",
+        "category",
+        "image",
+        "description",
+        "link",
+    ]
+
+    const isEmpty = valuesToCheck.find(function(value) {
+        const checkIfIsString = typeof event.target[value].value === "string";
+        const checkIsEmpty = !event.target[value].value.trim();
+        
+        if (checkIfIsString && checkIsEmpty) {
+            return true;
+        }
+
+        if (checkIfIsString && true) {
+            
+        }
+    })
+
+    if (isEmpty) {
+        event.preventDefault();
+        alert("Por favor, preecha todos os campos");
+    };
+    
+
+    // for (let value of valuesToCheck) {
+    //     console.log(event.target[value].value);
+    // }
+
+}
--- server.js
+++ server.js
@@ -4,54 +4,6 @@ const server = express();
 
 const db = require("./db");
 
-// // ************************************
-// const ideas = [
-//     {
-//         img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-//         title: "Cursos de Programação",
-//         category: "Estudo",
-//         description: "Curso completo de JavaScript, ReactJS, React Native e Banco de Dados.",
-//         url: "https://rocketseat.com.br"
-//     },
-//     {
-//         img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
-//         title: "Exercícios",
-//         category: "Saúde",
-//         description: "Atividades físicas para cuidar da saúde no período de quarentena.",
-//         url: "https://saude.gov.br"
-//     },
-//     {
-//         img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
-//         title: "Meditação",
-//         category: "Mentalidade",
-//         description: "Atividades para trabalhar o psicológico para enfrentar a morosidade de não sair de casa.",
-//         url: "https://rocketseat.com.br"
-//     },
-//     {
-//         img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
-//         title: "Karaokê",
-//         category: "Família",
-//         description: "Músicas para cantar com a família.",
-//         url: "https://rocketseat.com.br"
-//     },
-//     {
-//         img: "https://image.flaticon.com/icons/svg/2729/2729038.svg",
-//         title: "Pintura",
-//         category: "Criatividade",
-//         description: "Desenhos para as crianças colorir.",
-//         url: "https://rocketseat.com.br"
-//     },
-//     {
-//         img: "https://image.flaticon.com/icons/svg/2729/2729048.svg",
-//         title: "Recortes",
-//         category: "Criatividade",
-//         description: "Músicas para cantar com a família.",
-//         url: "https://rocketseat.com.br"
-//     },
-// ]
-// // ************************************
-
-
 // Configurando arquivos estáticos
 server.use(express.static("public"));
 
